5570c04fb2a8758f918dcf693f95e73428a920f2
unknown <cuzvin2@gmail.com>
2019-04-07 20:36:12 -0400

    everything displays; I notice a little editing on time still needs to happen

--- assets/Javascript/train.js
+++ assets/Javascript/train.js
@@ -53,41 +53,73 @@ $(".btn-success").on("click", function () {
 //  Then i can rename the variables and put them in HTML form at so as to get them on the HTML 
 //  page.
 
-database.ref().on("child_added", function (snapshot) {
-    var trainInfo = snapshot.val();
-    
-    frequency = snapshot.val().frequency;
+database.ref().on("child_added", function (childSnapshot) {
+    var trainInfo = childSnapshot.val();
+    console.log(typeof trainInfo.trainName);
+    frequency = childSnapshot.val().frequency;
     console.log(frequency);
+
+
     //working with moment.js
+// First Time (pushed back 1 year to make sure it comes before current time)
+var firstTimeConverted = moment(trainInfo.firsttime, "HH:mm").subtract(1, "years");
+console.log(firstTimeConverted);
+
     var now = moment();
     
-    console.log(now.toString());
-    var newTime = now.add({m: frequency});
-    newTime = (newTime.toString()); //.this is the variable I want to use for the next train arrival
-    $("tbody").append("<tr>").append("<td>" + trainInfo.trainName + "</td>").append("<td>" + trainInfo.destination + "</td>").append("<td>" + trainInfo.frequency + "</td>").append("<td>" + trainInfo.firsttime + "</td>" + "<td></td>");
+    console.log(now._d);
+    
+    // var newTime = now.add({m: frequency}); // I need the format hh.mm.ampm
+    //newTime = (newTime.toString()); //.this is the variable I want to use for the next train arrival
+    // var newTimeFormat = newTime.format("HH:mm A" );
+    var newDataName = $("<td>");
+    newDataName.text(trainInfo.trainName);
+
+    var newDataDest = $("<td>");
+    newDataDest.text(trainInfo.destination);
+
+    var newDataFreq = $("<td>");
+    newDataFreq.text(trainInfo.frequency);
+
+ // Difference between the times
+ var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+ console.log("DIFFERENCE IN TIME: " + diffTime);
+ 
+    // Getting remainder = total minutes % freq
+    var tRemainder = diffTime % frequency;
+    console.log(tRemainder);
+
+  // Minutes until next train
+  var tMinutesTilNextTrain = frequency - tRemainder;
+  console.log("Minutes til next train: " + tMinutesTilNextTrain);
+
+    var nextTrainArrives = $("<td>");
+    nextTrainArrives.text(now.add(tMinutesTilNextTrain));
+
+
+    $("tbody").append("<tr>").append(newDataName).append(newDataDest).append(newDataFreq).append(nextTrainArrives).append(tMinutesTilNextTrain);// + trainInfo.trainName + "</td>").append("<td>" + trainInfo.destination + "</td>").append("<td>" + trainInfo.frequency + "</td>").append("<td>" + trainInfo.firsttime + "</td>" + "<td></td>");
+
+
 
     });
-// database.ref().on("value", function (snapshot) {
-   // trnName = snapshot.val().trainName;
-    // destination = snapshot.val().destination;
-    // firstTime = snapshot.val().firsttime;
-// });
 
-    // This is an attempt at a for loop
-    // maybe I don't need one . . .from an ex I saw online, I can do another event listener
+ /*   
+// Difference between the times
+var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+console.log("DIFFERENCE IN TIME: " + diffTime);
 
-        
-/*
-        // I am not sure why the firebase isn't loading, so I will test my javascript knowledge.
-        // I will try putting the inputs directly to the html place
-                   
-        for (var i = 0; i < 4; i++) {
-                    var newDiv = $("<tr>");
-                    newDiv.append("<td>" + trnName + "</td>").append("<td>" + destination + "</td>").append("<td>" + firstTime + "</td>").append("<td>" + frequency + "</td>");
-                    $("tbody").append(newDiv);
-                }
+// Time apart (remainder)
+var tRemainder = diffTime % tFrequency;
+console.log(tRemainder);
+
+// Minute Until Train
+var tMinutesTillTrain = tFrequency - tRemainder;
+console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+// Next Train
+var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 */
-
 
 
 
