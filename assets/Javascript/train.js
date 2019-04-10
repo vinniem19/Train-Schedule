edfccdbc5c442b12d3a9418a94200199fc3e508f
unknown <cuzvin2@gmail.com>
2019-04-10 10:30:16 -0400

    I added background image and edited comments in code.

--- assets/Javascript/train.js
+++ assets/Javascript/train.js
@@ -48,10 +48,8 @@ $(".btn-success").on("click", function () {
     });
 });
 
-//  The information stored in the database is now in the form of an object. I have to use the 
-//  method to retrieve that info as an object and parse it from the object back into strings.
-//  Then i can rename the variables and put them in HTML form at so as to get them on the HTML 
-//  page.
+//  The information stored in the database; when a user adds a train, we need to keep every instance
+//  as a separate object
 
 database.ref().on("child_added", function (childSnapshot) {
     var trainInfo = childSnapshot.val();
@@ -70,9 +68,6 @@ console.log(firstTimeConverted);
     
     console.log(now._d);
     
-    // var newTime = now.add({m: frequency}); // I need the format hh.mm.ampm
-    //newTime = (newTime.toString()); //.this is the variable I want to use for the next train arrival
-    // var newTimeFormat = newTime.format("HH:mm A" );
     var newDataName = $("<td>");
     newDataName.text(trainInfo.trainName);
 
@@ -97,31 +92,16 @@ var minutesTillNextTrain = $("<td>");
     minutesTillNextTrain.text(tMinutesTilNextTrain);
 
     var nextTrainArrives = $("<td>");
-    nextTrainArrives.text(now.add(tMinutesTilNextTrain, "m").format("hh:mm"));
+    nextTrainArrives.text(now.add(tMinutesTilNextTrain, "m").format("hh:mm A"));
 
-    $("tbody").append("<tr>").append(newDataName).append(newDataDest).append(newDataFreq).append(nextTrainArrives).append(minutesTillNextTrain);// + trainInfo.trainName + "</td>").append("<td>" + trainInfo.destination + "</td>").append("<td>" + trainInfo.frequency + "</td>").append("<td>" + trainInfo.firsttime + "</td>" + "<td></td>");
+    // Display the trains data onto html page
+    $("tbody").append("<tr>").append(newDataName).append(newDataDest).append(newDataFreq).append(nextTrainArrives).append(minutesTillNextTrain);
 
 
 
     });
 
- /*   
-// Difference between the times
-var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-console.log("DIFFERENCE IN TIME: " + diffTime);
-
-// Time apart (remainder)
-var tRemainder = diffTime % tFrequency;
-console.log(tRemainder);
-
-// Minute Until Train
-var tMinutesTillTrain = tFrequency - tRemainder;
-console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-
-// Next Train
-var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
-*/
+ 
 
 
 
